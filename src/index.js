e7e93de5e0fc5f1d49ed9072046c201fe425a71c
joeldevel <christianjoel.developer@gmail.com>
2021-08-15 03:23:57 -0300

    buttons have id and class

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 console.log('works!');
-function square() {
+function square(id) {
+  let _id = id;
   function setMark(mark) {
     this.mark = mark;
   }
   function getMark() {
     return this.mark;
   }
-  return {setMark, getMark}
+  function getId() {
+    return _id;
+  }
+  return {setMark, getMark, getId}
+}
+
+function player(mark) {
+    function getMark() {
+      return this.mark;
+    }
+    return {getMark}
 }
 
 const gameboard = (function(){
     squares = [
-      square(),square(),square(),
-      square(),square(),square(),
-      square(),square(),square()
+      square(1),square(2),square(3),
+      square(4),square(5),square(6),
+      square(7),square(8),square(9)
     ];
 
     function getSquare(index) {
@@ -21,7 +32,10 @@ const gameboard = (function(){
     }
     function addSquare(where, square) {
       const btn = document.createElement('button');
-      btn.textContent = 'X';
+      btn.classList.add('gameboard-btn');
+      btn.setAttribute('id', square.getId());
+      btn.textContent =" ";
+      // console.log(square.getId());
       where.appendChild(btn);
     }
 
