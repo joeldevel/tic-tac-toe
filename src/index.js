76daa76336127ee31da6e521145746cd81ab8682
joeldevel <christianjoel.developer@gmail.com>
2021-08-15 02:57:41 -0300

    gameboard module and square factory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('works!');
+function square() {
+  function setMark(mark) {
+    this.mark = mark;
+  }
+  function getMark() {
+    return this.mark;
+  }
+  return {setMark, getMark}
+}
+
+const gameboard = (function(){
+    squares = [
+      square(),square(),square(),
+      square(),square(),square(),
+      square(),square(),square()
+    ];
+
+    function getSquare(index) {
+      return squares[index];
+    }
+    function addSquare(where, square) {
+      const btn = document.createElement('button');
+      btn.textContent = 'X';
+      where.appendChild(btn);
+    }
+
+    function displayGameboard(where) {
+      squares.forEach(square=>{
+        addSquare(where, square);
+      });
+    }
+    return {getSquare, displayGameboard};
+
+})();
+
+const displayGameboard = document.querySelector('.gameboard');
+
+gameboard.displayGameboard(displayGameboard);
